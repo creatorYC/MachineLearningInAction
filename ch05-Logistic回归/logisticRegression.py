3f769cf9cb31210108cf378356fb2476373ef0b9
creatorYC <1522120424@qq.com>
2018-04-19 11:09:41 +0800

    Logistic回归示例

--- ch05-Logistic回归/logisticRegression.py
+++ ch05-Logistic回归/logisticRegression.py
@@ -99,13 +99,60 @@ def plotBestFit(weights):
     plt.show()
 
 
+# ---------------------- 从疝气病症预测病马的死亡率 -----------------------------
+# Logistic 回归分类函数
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []
+    trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradientAscent_1(array(trainingSet), trainingLabels, 500)
+    errorCount = 0
+    numTestVect = 0.0
+    for line in frTest.readlines():
+        numTestVect += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount)/numTestVect)
+    print("the error rate is: %f" % errorRate)
+    return errorRate
+
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" 
+                        % (numTests, errorSum/float(numTests)))
+
 
 if __name__ == '__main__':
-    dataArr, labelMat = loadDataSet()
+    # dataArr, labelMat = loadDataSet()
     # weights = gradientAscent(dataArr, labelMat)
     # plotBestFit(weights.getA()) # getA(): 矩阵变量转化为ndarray类型的变量
-    weights = stocGradientAscent_1(array(dataArr), labelMat)
-    plotBestFit(weights)
+    # weights = stocGradientAscent_1(array(dataArr), labelMat)
+    # plotBestFit(weights)
+    multiTest()
 
 
 
