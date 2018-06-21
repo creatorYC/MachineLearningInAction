5f70c8f229072dbba53f80f85f52f8978b78ea12
creatorYC <1522120424@qq.com>
2018-06-21 17:28:52 +0800

    CART initial

---
+++ ch09-CART/regressionTrees.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+from numpy import *
+
+
+def loadDataSet(filename):
+    dataMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = map(float, curLine)
+        dataMat.append(fltLine)
+    return dataMat
+
+
+# 通过数组过滤方式切分数据集合
+def binSplitDataSet(dataSet, featureIndex, value):
+    mat0 = dataSet[nonzero(dataSet[:, featureIndex] > value)[0], :][0]
+    mat1 = dataSet[nonzero(dataSet[:, featureIndex] <= value)[0], :][0]
+    return mat0, mat1
+
+
+def regLeaf(dataSet):
+    return mean(dataSet[:, -1])
+
+
+def regErr(dataSet):
+    return var(dataSet[:, -1]) * shape(dataSet)[0]
+
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    tolS = ops[0]
+    tolN = ops[1]
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
+        return None, leafType(dataSet)
+    m, n = shape(dataSet)
+    S = errType(dataSet)
+    bestS = inf
+    bestIndex = 0
+    bestValue = 0
+    for featIndex in range(n-1):
+        for splitVal in set(dataSet[:, featIndex]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+                continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    if(shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+        return None, leafType(dataSet)
+    return bestIndex, bestValue
+
+
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    feature, value = chooseBestSplit(dataSet, leafType, errType, ops)
+    if feature == None:
+        return value
+    retTree = {}
+    retTree['spInd'] = feature
+    retTree['spVal'] = value
+    lSet, rSet = binSplitDataSet(dataSet, feature, value)
+    retTree['left'] = createTree(lSet, leafType, errType, ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops)
+    return retTree
