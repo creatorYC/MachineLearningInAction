3ae908cbfa7e2e05182c32b1cf0a51ac023b2aa3
creatorYC <1522120424@qq.com>
2018-06-22 11:25:04 +0800

    构建树

--- ch09-CART/regressionTrees.py
+++ ch09-CART/regressionTrees.py
@@ -9,14 +9,14 @@ def loadDataSet(filename):
     for line in fr.readlines():
         curLine = line.strip().split('\t')
         fltLine = map(float, curLine)
-        dataMat.append(fltLine)
+        dataMat.append(list(fltLine))
     return dataMat
 
 
 # 通过数组过滤方式切分数据集合
 def binSplitDataSet(dataSet, featureIndex, value):
-    mat0 = dataSet[nonzero(dataSet[:, featureIndex] > value)[0], :][0]
-    mat1 = dataSet[nonzero(dataSet[:, featureIndex] <= value)[0], :][0]
+    mat0 = dataSet[nonzero(dataSet[:, featureIndex] > value)[0], :]
+    mat1 = dataSet[nonzero(dataSet[:, featureIndex] <= value)[0], :]
     return mat0, mat1
 
 
@@ -28,10 +28,12 @@ def regErr(dataSet):
     return var(dataSet[:, -1]) * shape(dataSet)[0]
 
 
+# 寻找数据的最佳二元切分方式
 def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
-    tolS = ops[0]
-    tolN = ops[1]
-    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
+    tolS = ops[0]   # 容许的误差下降值
+    tolN = ops[1]   # 切分的最少样本数
+    # tolist: 矩阵转换成列表,矩阵是一维的时候有tolist()[0]
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:     # 如果所有feature值相等则退出
         return None, leafType(dataSet)
     m, n = shape(dataSet)
     S = errType(dataSet)
@@ -39,7 +41,7 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
     bestIndex = 0
     bestValue = 0
     for featIndex in range(n-1):
-        for splitVal in set(dataSet[:, featIndex]):
+        for splitVal in set(dataSet[:, featIndex].T.tolist()[0]):
             mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
             if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
                 continue
@@ -48,22 +50,30 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
                 bestIndex = featIndex
                 bestValue = splitVal
                 bestS = newS
-    if (S - bestS) < tolS:
+    if (S - bestS) < tolS:  # 如果误差减少不大则退出
         return None, leafType(dataSet)
     mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    # 如果切分出的数据集很小则退出
     if(shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
         return None, leafType(dataSet)
     return bestIndex, bestValue
 
 
 def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
-    feature, value = chooseBestSplit(dataSet, leafType, errType, ops)
-    if feature == None:
+    featureIndex, value = chooseBestSplit(dataSet, leafType, errType, ops)
+    if featureIndex == None:
         return value
     retTree = {}
-    retTree['spInd'] = feature
+    retTree['spIdx'] = featureIndex
     retTree['spVal'] = value
-    lSet, rSet = binSplitDataSet(dataSet, feature, value)
+    lSet, rSet = binSplitDataSet(dataSet, featureIndex, value)
     retTree['left'] = createTree(lSet, leafType, errType, ops)
     retTree['right'] = createTree(rSet, leafType, errType, ops)
     return retTree
+
+
+if __name__ == '__main__':
+    myData = loadDataSet('ex0.txt')
+    myMat = mat(myData)
+    retTree = createTree(myMat)
+    print(retTree)
