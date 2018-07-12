5c36b0e5adc5ce13ce86ae188b4070ac603c1198
creatorYC <1522120424@qq.com>
2018-07-12 15:45:03 +0800

    构建FP树

---
+++ ch12-FP-growth/fpGrowth.py
@@ -0,0 +1,99 @@
+# coding: utf-8
+
+from numpy import *
+
+
+# FP 树的类定义
+class treeNode:
+
+    def __init__(self, nameValue, numOccur, parentNode):
+        self.name = nameValue
+        self.count = numOccur
+        self.nodeLink = None
+        self.parent = parentNode
+        self.children = {}
+
+    def inc(self, numOccur):
+        self.count += numOccur
+
+    def display(self, ind=1):
+        print(' '*ind, self.name, ' ', self.count)
+        for child in self.children.values():
+            child.display(ind+1)
+
+
+def createTree(dataSet, minSup=1):
+    headerTable = {}
+    # 第一次遍历数据集，统计每个元素项出现的频度
+    for trans in dataSet:
+        for item in trans:
+            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+    # 移除不满足最小支持度的元素项
+    for k in list(headerTable.keys()):
+        if headerTable[k] < minSup:
+            del(headerTable[k])
+    freqItemSet = set(headerTable.keys())
+    if len(freqItemSet) == 0:
+        return None, None
+    for k in headerTable:
+        headerTable[k] = [headerTable[k], None]
+    retTree = treeNode('Null Set', 1, None)
+    for transSet, count in dataSet.items():
+        localD = {}
+        for item in transSet:
+            if item in freqItemSet:
+                localD[item] = headerTable[item][0]
+        if len(localD) > 0:  # 根据全局频率对每个事务中的元素进行排序
+            orderedItems = [v[0] for v in sorted(
+                localD.items(),
+                key=lambda p : p[1],
+                reverse=True
+            )]
+            # 使用排序后的频率项集对树进行填充
+            updateTree(orderedItems, retTree, headerTable, count)
+    return retTree, headerTable
+
+
+def updateTree(items, inTree, headerTable, count):
+    if items[0] in inTree.children:
+        inTree.children[items[0]].inc(count)
+    else:
+        inTree.children[items[0]] = treeNode(items[0], count, inTree)
+        if headerTable[items[0]][1] == None:
+            headerTable[items[0]][1] = inTree.children[items[0]]
+        else:
+            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+    if len(items) > 1:
+        updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+
+
+def updateHeader(nodeToTest, targetNode):
+    while nodeToTest.nodeLink != None:
+        nodeToTest = nodeToTest.nodeLink
+    nodeToTest.nodeLink = targetNode
+
+
+def loadSimpleData():
+    simpleData = [
+        ['r', 'z', 'h', 'j', 'p'],
+        ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'],
+        ['z'],
+        ['r', 'x', 'n', 'o', 's'],
+        ['y', 'r', 'x', 'z', 'q', 't', 'p'],
+        ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']
+    ]
+    return simpleData
+
+
+def createInitSet(dataSet):
+    retDict = {}
+    for trans in dataSet:
+        retDict[frozenset(trans)] = 1
+    return retDict
+
+
+if __name__ == '__main__':
+    simpData = loadSimpleData()
+    initSet = createInitSet(simpData)
+    myFPTree, myHeaderTable = createTree(initSet, 3)
+    myFPTree.display()
